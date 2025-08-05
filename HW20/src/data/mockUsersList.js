6d58cc0b25a8bd6209653d412c6863c6548dbd20
Mykyta_Mielkov <nikitajohnson07.02@gmail.com>
2025-08-05 16:56:20 +0200

    added HW20

---
+++ HW20/src/data/mockUsersList.js
@@ -0,0 +1,19 @@
+const mockUsersList = [
+    {
+        id: "user001",
+        name: "Alice",
+        email: "alice@example.com"
+    },
+    {
+        id: "user002",
+        name: "Bob",
+        email: "bob@example.com"
+    },
+    {
+        id: "user003",
+        name: "Charlie",
+        email: "charlie@example.com"
+    },
+];
+
+export default mockUsersList;
