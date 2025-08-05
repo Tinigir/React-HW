176165ca6fb6389289b5f4b045bc3c8af29d0f78
Mykyta_Mielkov <nikitajohnson07.02@gmail.com>
2025-08-05 17:32:35 +0200

    added HW23

---
+++ HW23/src/shared/components/PageTitle/PageTitle.jsx
@@ -0,0 +1,9 @@
+import styles from "./PageTitle.module.css";
+
+const PageTitle = ({ children }) => {
+    return (
+        <h1 className={styles.heading}>{children}</h1>
+    )
+};
+
+export default PageTitle;
